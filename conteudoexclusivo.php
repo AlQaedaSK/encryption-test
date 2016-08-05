9b9bcd045a1cfb4255afe50e1d5a0c11dafc0788
Jonas Lopes <Jonas Lopes>
2016-08-04 22:39:26 -0300

    Front-end inicial

---
+++ conteudoexclusivo.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+
+<html lang="br" class="no-js">
+
+<head>
+
+<meta charset="utf-8">
+<title>Sistema de Login e Senha Criptografados</title>
+<link href="style.css" rel="stylesheet" />
+
+</head>
+
+<body>
+
+<div id="conteudo">
+
+<h1><span class="destaca">"Nome"</span>, seja bem-vindo ao Conteúdo Exclusivo</h1>
+
+<div class="borda"></div>
+
+<div class="clear"></div>
+
+<h3>Conteúdo exclusivo 01</h3>
+
+<p>"Info 1"</p>
+
+
+<h3>Conteúdo Exclusivo 02</h3>
+
+<p>"Info 2"</p>
+
+<div class="borda"></div>
+
+</div>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+
+<html lang="br" class="no-js">
+
+<head>
+
+<meta charset="utf-8">
+<title>Sistema de Registro Criptografado</title>
+<link href="style.css" rel="stylesheet" />
+
+</head>
+
+<body>
+
+<div id="conteudo">
+
+<h1>Sistema de registro criptografado</h1>
+
+<div class="borda"></div>
+
+<form method="post" action="registra.php" id="registra">
+<fieldset>
+<legend>Faça seu registo abaixo!</legend>
+<label for="nome">Seu nome:</label>
+<input type="text" name="nome" id="nome" />
+<div class="clear"></div>
+<label for="email">E-mail:</label>
+<input type="text" name="email" id="email" />
+<div class="clear"></div>
+<label for="info1">Info 1:</label>
+<input type="text" name="info1" id="info1" />
+<div class="clear"></div>
+<label for="senha">Info 2:</label>
+<input type="text" name="info2" id="info2" />
+<div class="clear"></div>
+<input type="submit" value="Efetuar resgistro" />
+
+</fieldset>
+</form>
+
+</div>
+
+</body>
+
+</html>
